3b8b69634828316daddc6549cfc620777f931f6f
ntvinh11586 <ntvinh.11586@gmail.com>
2016-10-02 00:55:31 +0700

    Initial commit

---
+++ app/src/main/java/com/coderschool/vinh/todoapp/DatabaseHandler.java
@@ -0,0 +1,105 @@
+package com.coderschool.vinh.todoapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.coderschool.vinh.todoapp.models.SQLPackage;
+
+import java.util.ArrayList;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "packagesManager";
+    private static final String TABLE_PACKAGES = "packages";
+
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PRIORITY = "priority";
+    private static final String KEY_DAY = "day";
+    private static final String KEY_MONTH = "month";
+    private static final String KEY_YEAR = "year";
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_PACKAGES_TABLE = "CREATE TABLE " + TABLE_PACKAGES + "("
+                + KEY_ID + " INTEGER PRIMARY KEY,"
+                + KEY_NAME + " TEXT,"
+                + KEY_PRIORITY + " TEXT,"
+                + KEY_DAY + " INTEGER,"
+                + KEY_MONTH + " INTEGER,"
+                + KEY_YEAR + " INTEGER" + ")";
+
+        db.execSQL(CREATE_PACKAGES_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PACKAGES);
+
+        onCreate(db);
+    }
+
+    public void addPackage(SQLPackage sqlPackage) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(KEY_NAME, sqlPackage.name);
+        values.put(KEY_PRIORITY, sqlPackage.priority);
+        values.put(KEY_DAY, sqlPackage.day);
+        values.put(KEY_MONTH, sqlPackage.month);
+        values.put(KEY_YEAR, sqlPackage.year);
+
+        db.insert(TABLE_PACKAGES, null, values);
+        db.close();
+    }
+
+    public ArrayList<SQLPackage> getAllPackages() {
+        ArrayList<SQLPackage> packageList = new ArrayList<SQLPackage>();
+
+        String selectQuery = "SELECT * FROM " + TABLE_PACKAGES;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                SQLPackage sqlPackage = new SQLPackage();
+
+                sqlPackage.id = Integer.parseInt(cursor.getString(0));
+                sqlPackage.name = cursor.getString(1);
+                sqlPackage.priority = cursor.getString(2);
+                sqlPackage.day = Integer.parseInt(cursor.getString(3));
+                sqlPackage.month = Integer.parseInt(cursor.getString(4));
+                sqlPackage.year = Integer.parseInt(cursor.getString(5));
+
+                packageList.add(sqlPackage);
+            } while (cursor.moveToNext());
+        }
+
+        return packageList;
+    }
+
+    public void deleteAllPackages() {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            db.delete(TABLE_PACKAGES, null, null);
+            db.setTransactionSuccessful();
+        } catch (Exception e) {
+
+        } finally {
+            db.endTransaction();
+        }
+    }
+}
